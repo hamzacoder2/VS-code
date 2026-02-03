d88873618db320a9fd08fef8f08d709f4e7feff4
hamzacoder2 <hamzaeac@gmail.com>
2026-02-03 21:24:57 +0500

    intro

---
+++ Python/intro.py
@@ -0,0 +1,7 @@
+#print hello world
+'''This is my first pgm in python coding'''
+print("welcome to world of programming\n list of keywords:")
+print("sum of 2 numbers",11*3)
+
+import keyword
+print(keyword.kwlist)
