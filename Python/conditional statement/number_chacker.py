5428372dd495fe28e6d222c65c6f39cadeda1f1c
hamzacoder2 <hamzaeac@gmail.com>
2026-02-17 21:02:51 +0500

    number_chacker

---
+++ Python/conditional statement/number_chacker.py
@@ -0,0 +1,8 @@
+i=int(input("Enter a number:    "))
+if(i<50):
+    print("i is smaller that 50")
+    print("i'm in if block")
+else:
+    print("i is greater than 50")
+    print("i'm in else block")
+print("I'm not in if and not in else block")
