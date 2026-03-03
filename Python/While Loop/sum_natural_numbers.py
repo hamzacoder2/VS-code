7345f4efe96b9f0b5f61baaac37a184e43435da3
hamzacoder2 <hamzaeac@gmail.com>
2026-03-03 20:48:53 +0500

    sum_natural_numbers

---
+++ Python/While Loop/sum_natural_numbers.py
@@ -0,0 +1,10 @@
+#input the value of terms
+n = int(input("Enter the value of the term : "))
+
+sum = 0 #initialize
+i = 1 #initialize
+while i<=n:#Loop will run from 1 to n
+  sum = sum+i
+  i = i+1
+
+print("\nSum=",sum)
